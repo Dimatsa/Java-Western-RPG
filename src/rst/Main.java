a875203020a2453b6fb0cb7a7d00d3123dfce99d
Dmitry Tsarapkine <dimazonyoutubing@gmail.com>
2019-06-10 14:00:51 -0400

    Textures & Scenes

--- src/rst/Main.java
+++ src/rst/Main.java
@@ -10,27 +10,25 @@ public class Main {
 	public static void main(String[] args) {
 		createAndShowGUI();
 
-		//Creates Dalton brothers
-		Character slimy = new Character("Slimy", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character sneaky = new Character("Sneaky", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character shifty = new Character("Shifty", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character snitchy = new Character("Snitchy", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1);
+		// Creates Dalton brothers
+		Character slimy = new Character("Slimy", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character sneaky = new Character("Sneaky", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character shifty = new Character("Shifty", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character snitchy = new Character("Snitchy", "Dalton", Character.MALE, 1, 1, 1, 1, 1, 1, null);
 
-		//Creates Earps
-		Character marshallVirgil = new Character("Marshal Virgil", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character wyatt = new Character("Wyatt", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character morgan = new Character("Morgan", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character docHolliday = new Character("Doc Holliday", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1);
+		// Creates Earps
+		Character marshallVirgil = new Character("Marshal Virgil", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character wyatt = new Character("Wyatt", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character morgan = new Character("Morgan", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character docHolliday = new Character("Doc Holliday", "Earp", Character.MALE, 1, 1, 1, 1, 1, 1, null);
 
-		//Creates random characters
-		Character ryan = new Character("Ryan", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character toby = new Character("Toby", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character gian = new Character("Giancarlo", "Salvador", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character steve = new Character("Steve", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1);
-		Character flinston = new Character("Flinston", "Stone", Character.MALE, 1, 1, 1, 1, 1, 1);
+		// Creates random characters
+		Character ryan = new Character("Ryan", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character toby = new Character("Toby", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character gian = new Character("Giancarlo", "Salvador", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character steve = new Character("Steve", "Larkin", Character.MALE, 1, 1, 1, 1, 1, 1, null);
+		Character flinston = new Character("Flinston", "Stone", Character.MALE, 1, 1, 1, 1, 1, 1, null);
 
-
-		
 	}
 
 	private static void createAndShowGUI() {
---
+++ src/rst/scene/Saloon.java
@@ -0,0 +1,5 @@
+package rst.scene;
+
+public class Saloon {
+
+}
