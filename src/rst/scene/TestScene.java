52769f444247fa3a46fcfb43bc6e2b8ebeb008dd
Dmitry Tsarapkine <dimazonyoutubing@gmail.com>
2019-06-10 14:30:07 -0400

    Block Creation

--- src/rst/scene/TestScene.java
+++ src/rst/scene/TestScene.java
@@ -1,27 +1,49 @@
 package rst.scene;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rst.assets.AssetRegistry;
 import rst.character.Characters;
 import rst.render.Block;
 import rst.render.CameraFollowable;
-import rst.render.HittableBlock;
+import rst.render.Renderable;
 
 public class TestScene extends Scene {
 
 	public TestScene() {
 		super(AssetRegistry.getTextures().get("sand"), Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, AssetRegistry.getSounds().get("test"),
 				(CameraFollowable) Characters.getCharacters().getCharacter("Connor Adams"), 
-				"Test Scene", Characters.getCharacters().getCharacter("Connor Adams"),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 0, 0),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 9, 0),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 0, 9),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 9, 9),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 0, 1),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 2, 3),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 2, 5),
-				new HittableBlock(Block.GRID_SIZE * 10, Block.GRID_SIZE * 10, "path", 5, 3),
-				new Block("sand", 1, 0),
-				new Block("sand", 2, 0),
-				new Block("sand", 3, 0));
-	}
+				"Test Scene", makeBlocks(10, 10), Characters.getCharacters().getCharacter("Connor Adams"));
+				
+		
+}
+	
+		private static Block[] makeBlocks(int length, int width) {
+			Block[][] blocks = new Block[length][width];
+			
+			
+			return collapseArray(blocks);
+		}
+		
+		private static Block[] collapseArray(Block[][] blocks) {
+			List<Block> collapsed = new ArrayList<Block>();
+			
+			for(int i = 0; i < blocks.length; i++) {
+				for(int j = 0; j < blocks[i].length; j++) {
+					if(blocks[i][j] != null) {
+						collapsed.add(blocks[i][j]);
+					}
+				}
+			}
+			
+			return collapsed.toArray(new Block[0]);
+		}
+		
+		private Block[] combine(Renderable[] items, Renderable... others) {
+			Renderable[] combined = new Renderable[items.length + others.length];
+			
+			System.arraycopy(items, 0, combined, 0, items.length);
+			System.arraycopy(others, 0, , destPos, length);
+		}
 }
